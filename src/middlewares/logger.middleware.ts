51f2fb01b56cd1a693403d5d8f523dc4f9161171
Chesar <cesargarcialeoc@gmail.com>
2025-04-06 09:22:34 -0300

    Updated Files

--- src/middlewares/logger.middleware.ts
+++ src/middlewares/logger.middleware.ts
@@ -4,7 +4,7 @@ import { NextFunction, Request, Response } from 'express';
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    const currentDate = new Date().toISOString().split('T')[0]; // Only the date part
+    const currentDate = new Date().toISOString().split('T')[0];
     console.log(
       `[${currentDate}] Estás ejecutando un método ${req.method} en la ruta ${req.url}`
     );
@@ -13,7 +13,7 @@ export class LoggerMiddleware implements NestMiddleware {
 }
 
 export function loggerGlobal(req: Request, res: Response, next: NextFunction) {
-    const currentDate = new Date().toISOString().split('T')[0]; // Only the date part
+    const currentDate = new Date().toISOString().split('T')[0]; 
     console.log(`[${currentDate}] Estás ejecutando un método ${req.method} en la ruta ${req.url}`);
     next();
 }
